50f99f3e90d6f162dd1004cb2a695836bdf8081e
drsam3718 <deepsambhavani3718@gmail.com>
2023-03-26 00:40:19 +0530

    this is the first commit. I have created the first feature and navigation between three activites

---
+++ app/src/main/java/com/example/yourwardrobe/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.yourwardrobe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickOnClothsButton(View view){
+        Intent intent = new Intent(this, Cloths.class);
+        startActivity(intent);
+    }
+}
