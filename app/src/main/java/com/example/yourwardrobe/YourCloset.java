50a9c7bdb137570aa178a331b1ec45eea5647e3f
dipsambhavani <dip.sambhavani@gmail.com>
2023-05-18 12:26:48 +0530

    Revert "modified sorting by utilizing select query for each category"
    This reverts commit 1c31ac29a53b1303746d32b6da9d8d3e6d8fbd40.

--- app/src/main/java/com/example/yourwardrobe/YourCloset.java
+++ app/src/main/java/com/example/yourwardrobe/YourCloset.java
@@ -68,9 +68,10 @@ public class YourCloset extends AppCompatActivity {
                     mainClothsGrid.setAdapter(clothsAdapter);
                 }else{
                     clothsArrayListByCategory.clear();
-                    List<Cloths> clothsList = db.getByCategory(sort_by_spinner.getSelectedItem().toString());
-                    for(Cloths cloths: clothsList){
-                        clothsArrayListByCategory.add(cloths);
+                    for (Cloths cloths : clothsList) {
+                        if (cloths.getCategory().equals(sort_by_spinner.getSelectedItem().toString())){
+                            clothsArrayListByCategory.add(cloths);
+                        }
                     }
                     ClothsAdapter clothsAdapterByCategory = new ClothsAdapter(YourCloset.this, clothsArrayListByCategory);
                     mainClothsGrid.setAdapter(clothsAdapterByCategory);
